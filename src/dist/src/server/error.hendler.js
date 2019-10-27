4c6739397dc9311c63fc1b9804862c52ef89d893
ederpbj <ederpbj@yahoo.com.br>
2019-10-27 17:46:20 -0300

    A34. Validadores Padrões – Mongoose

---
+++ src/dist/src/server/error.hendler.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleError = (req, resp, err, done) => {
+    //console.log(err)
+    //restify procura por esse método, vamos sobrescrever
+    err.toJSON = () => {
+        return {
+            message: err.message
+        };
+    };
+    switch (err.name) {
+        case 'MongoError':
+            if (err.code === 11000) {
+                err.statusCode = 400;
+            }
+            break;
+        case 'ValidationError':
+            err.statusCode = 400;
+            const messages = [];
+            for (let name in err.errors) {
+                messages.push({ message: err.errors[name].message });
+                //console.log(err.errors[name])
+            }
+            err.toJSON = () => ({
+                errors: messages,
+            });
+            break;
+    }
+    done();
+};
