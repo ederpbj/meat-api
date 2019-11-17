a77097461225c76e6bfabd34746eaf3e63643a41
ederpbj <ederpbj@yahoo.com.br>
2019-11-17 14:57:32 -0300

    A46. Adicionando Links na API

---
+++ Sessões/A46. Adicionando Links na API/src/dist/src/server/server.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const mongoose = require("mongoose");
+const environment_1 = require("../common/environment");
+const merge_patch_parser_1 = require("./merge-patch.parser");
+const error_hendler_1 = require("./error.hendler");
+class Server {
+    initializeDb() {
+        mongoose.Promise = global.Promise;
+        return mongoose.connect(environment_1.environment.db.url, {
+            useMongoClient: true
+        });
+    }
+    initRoutes(routers) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.application = restify.createServer({
+                    name: 'meat-api',
+                    version: '1.0.0'
+                });
+                //Plugins:
+                this.application.use(restify.plugins.queryParser());
+                this.application.use(restify.plugins.bodyParser());
+                //Recomendação para application/merge-patch+json
+                this.application.use(merge_patch_parser_1.mergePatchBodyParser);
+                //routes
+                for (let router of routers) {
+                    router.applyRoutes(this.application);
+                }
+                this.application.listen(environment_1.environment.server.port, () => {
+                    resolve(this.application);
+                });
+                //Tratamento de erro
+                //A33. Tratamento de Erros com Restify
+                this.application.on('restifyError', error_hendler_1.handleError);
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    bootstrap(routers = []) {
+        return this.initializeDb().then(() => this.initRoutes(routers).then(() => this));
+    }
+}
+exports.Server = Server;
